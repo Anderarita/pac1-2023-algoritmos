b219a2344cbac4e08bb7829afe06125a2c936053
Anderarita <123851437+Anderarita@users.noreply.github.com>
2023-02-01 22:40:45 -0600

    Update Promedio.java

--- Unidad I/ejercicios/basicos2/src/estructurasControl/Promedio.java
+++ Unidad I/ejercicios/basicos2/src/estructurasControl/Promedio.java
@@ -16,11 +16,21 @@ public class Promedio{
         double promedio = (Math.abs(nota)+ Math.abs(notaa)+ Math.abs(notas)) / Math.abs(3);
         System.out.println("el Promedio Es De: "+ promedio);
         
-        if (promedio >= 70) {
+        if (promedio  < 70 ) {
             
-            System.out.println("El Estudiante Paso: ");
+            System.out.println("El Estudiante Reprobo: ");
             
-        }else{
+        }else if (promedio > 70 && promedio <=80) {
+            System.out.println("El Estudiante Aprobo con nota: " + promedio);
+            
+        }else if (promedio > 80 && promedio <= 90) {
+            System.out.println("El estudiante aprobo con nota muy buena: "+ promedio);
+            
+        }else if (promedio > 90 && promedio <=100) {
+            System.out.println("el EStudiante aprobo con nota sobresaliente: "+promedio);
+            
+        }else
+{
             System.out.println("El Estudiante DEjo Algoritmos: ");
         }
         
